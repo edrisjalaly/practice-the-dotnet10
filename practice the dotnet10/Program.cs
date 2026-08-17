67af21efd0ebf4e59b9b538e299b4f5e8ced3c3e
edris <edrisjalaly2018@email.com>
2026-08-17 09:29:17 +0430

    variables declration and assignment

--- practice the dotnet10/Program.cs
+++ practice the dotnet10/Program.cs
@@ -1,4 +1,5 @@
 ﻿Console.WriteLine("Hello, World!");
+
 Console.WriteLine("size of sbyte: " + sizeof(sbyte));
 Console.WriteLine("size of byte: " + sizeof(byte));
 Console.WriteLine("size of short: " + sizeof(short));
@@ -7,3 +8,18 @@ Console.WriteLine("size of int: " + sizeof(int));
 Console.WriteLine("size of uint: " + sizeof(uint));
 Console.WriteLine("size of long: " + sizeof(long));
 Console.WriteLine("size of ulong: " + sizeof(ulong));
+
+
+// declaraction and assignment of a variable
+// type [variable name];
+// type [variable name] = [value];
+
+int totalProducts = 100;
+long views = 500;
+float price = 19.99f;
+double result = 15000.80002;
+decimal balance = 100000.50m;
+bool isActive = true;
+char grade = 'A';
+string message = "Hello, World!";
+var name = "i well call you latter";
