6ff4c1adf5962510801048534215fcbef30a2125
edris <edrisjalaly2018@email.com>
2026-08-17 09:20:53 +0430

    Add project files.

---
+++ practice the dotnet10/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("size of sbyte: " + sizeof(sbyte));
+Console.WriteLine("size of byte: " + sizeof(byte));
+Console.WriteLine("size of short: " + sizeof(short));
+Console.WriteLine("size of ushort: " + sizeof(ushort));
+Console.WriteLine("size of int: " + sizeof(int));
+Console.WriteLine("size of uint: " + sizeof(uint));
+Console.WriteLine("size of long: " + sizeof(long));
+Console.WriteLine("size of ulong: " + sizeof(ulong));
